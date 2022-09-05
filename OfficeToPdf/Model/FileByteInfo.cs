8b9004e10ecbf1a280dbe981be34dacfd894410a
DESKTOP-E91V2B7\win <891367701@qq.com>
2022-09-05 14:38:01 +0800

    'up'

---
+++ OfficeToPdf/Model/FileByteInfo.cs
@@ -0,0 +1,10 @@
+using System;
+namespace OfficeToPdf
+{
+    public class FileByteInfo
+    {
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public Byte[] Bytes { get; set; }
+    }
+}
